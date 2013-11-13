84b8931b26a02f78a7a6b740c854b7f5e3e339e9
jordanhall916 <jordanhall@126.com>
2013-11-13 10:26:47 +0800

    1、在.c文件中包含了string.h文件 2、在.h文件中包含了gsm_type.h文件 3、宏定义GSM_COMM_SEL预留了扩展通信USART的接口 4、增加了USART_GPIO配置函数void GSM_USART_GPIO(void); 5、增加了USART配置函数GSM_USART_Config(void); 6、增加了USART中断配置函数GSM_USART_NVIC(void); 7、增加了串口发送一个字节函数GSM_USART_TxChar(UINT8 c); 8、增加了串口发送数据函数GSM_USART_TxData(UINT8 *data, int len); 9、增加了串口发送字符串函数SM_USART_TxStr(char *str); 10、增加了串口接收数据函数void GSM_USART_Rx(void); 11、增加了串口初始化函数void GSM_USART_Init(void);

--- APP/sim900/gsm_driver.c
+++ APP/sim900/gsm_driver.c
@@ -1,14 +1,15 @@
 /**
  * GSM模块与STM32的接口层
  *
- * @author 王特
+ * @author 王特、矫东航
  */
 
 #include "gsm_driver.h"
 
+#include <string.h>
+
 #include "stm32f10x_gpio.h"
 #include "stm32f10x_tim.h"
-
 
 void GSM_TimeInit(void)
 {
@@ -91,4 +92,108 @@ void GSM_TimeHandle(void)
 			GSM_ShutTime();
 		}
 	}
+}
+
+void GSM_USART_GPIO(void)
+{
+	GPIO_InitTypeDef GPIO_InitStructure;
+	
+#if (GSM_COMM_SEL == GSM_USART_1)
+	GPIO_PinRemapConfig(GPIO_Remap_USART1, ENABLE); 
+
+	RCC_APB2PeriphClockCmd( RCC_APB2Periph_USART1, ENABLE);
+	// 全双工发送PB6
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+	// 全双工接收PB7
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_7;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+#endif
+}
+
+void GSM_USART_Config(void)
+{
+	USART_InitTypeDef USART_InitStructure;
+	
+	USART_InitStructure.USART_BaudRate = 115200;
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;
+	USART_InitStructure.USART_Parity = USART_Parity_No;
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_InitStructure.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;
+#if (GSM_COMM_SEL == GSM_USART_1)	
+	USART_Init(USART1, &USART_InitStructure);
+	// 使能USART1外设
+	USART_Cmd(USART1, ENABLE);
+#endif
+}
+
+void GSM_USART_NVIC(void)
+{
+	NVIC_InitTypeDef NVIC_InitStructure; 
+	
+#if (GSM_COMM_SEL == GSM_USART_1)	
+	NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 3;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1; 
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&NVIC_InitStructure);	
+	
+	USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);	
+#endif
+}
+
+void GSM_USART_TxChar(UINT8 c)
+{
+#if (GSM_COMM_SEL == GSM_USART_1)
+	USART_SendData(USART1, c);
+	while(USART_GetFlagStatus(USART1, USART_FLAG_TC)==RESET);
+#endif	
+}
+
+void GSM_USART_TxData(UINT8 *data, int len)
+{
+	UINT8 *p = data;
+	while(len-- > 0) {
+		GSM_USART_TxChar(*p);
+		++p;
+	}
+}
+
+void GSM_USART_TxStr(char *str)
+{
+	GSM_USART_TxData((UINT8 *) str, strlen(str));
+}
+
+void GSM_USART_Rx(void)
+{
+	USART_TypeDef	*usart;
+	unsigned char	rx_data;
+	
+#if (GSM_COMM_SEL == GSM_UART_1)
+    usart = USART1;
+#endif
+
+	// 溢出-如果发生溢出需要先读SR,再读DR寄存器则可清除不断入中断的问题
+	if(USART_GetFlagStatus(usart, USART_FLAG_ORE) == SET){
+		USART_ClearFlag(usart, USART_FLAG_ORE);		// 读SR其实就是清除标志
+		USART_ReceiveData(usart);					// 读DR 
+	} 
+	if (USART_GetITStatus(usart, USART_IT_RXNE) != RESET) {
+		rx_data = USART_ReceiveData(usart) & 0xFF; 	
+		//GSM_RxHandler(rx_data);
+		// 有可能这个函数还没有执行完，数据又到来，就会产生USART_FLAG_ORE中断	
+		USART_ClearITPendingBit(usart, USART_IT_RXNE);
+	}
+}
+
+void GSM_USART_Init(void)
+{
+	GSM_USART_GPIO();
+	GSM_USART_Config();
+	GSM_USART_NVIC();
 }
--- APP/sim900/gsm_driver.h
+++ APP/sim900/gsm_driver.h
@@ -1,7 +1,7 @@
 /**
  * GSM模块与STM32的接口层
  *
- * @author 王特
+ * @author 王特、矫东航
  */
 
 #ifndef __USR_GSM_DRIVER_H__
@@ -9,6 +9,11 @@
 
 #include "stm32f10x.h"
 #include "stm32f107.h"
+
+#include "gsm_type.h"
+
+// 选择通信的USART
+#define GSM_COMM_SEL	GSM_USART_1
 
 // 初始化TIM2
 void GSM_TimeInit(void);
@@ -22,5 +27,20 @@ void GSM_SetTimeData(void);
 void GSM_ShutTime(void);
 // TIM更新事件处理
 void GSM_TimeHandle(void);
-
+// USART_GPIO配置
+void GSM_USART_GPIO(void);
+// USART配置
+void GSM_USART_Config(void);
+// USART中断配置
+void GSM_USART_NVIC(void);
+// 串口发送一个字节
+void GSM_USART_TxChar(UINT8 c);
+// 串口发送数据
+void GSM_USART_TxData(UINT8 *data, int len);
+// 串口发送字符串
+void GSM_USART_TxStr(char *str);
+// 串口接收数据
+void GSM_USART_Rx(void);
+// 串口初始化
+void GSM_USART_Init(void);
 #endif
