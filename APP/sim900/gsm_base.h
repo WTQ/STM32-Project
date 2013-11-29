802665cc919dcc151df787cde2bc5f81992cc0b3
jordanhall916 <jordanhall@126.com>
2013-11-29 10:46:22 +0800

    1、增加了接收期待字符串函数 2、修改去掉回车函数为自适应去掉回车函数 3、更正了一些错误

--- APP/sim900/gsm_base.h
+++ APP/sim900/gsm_base.h
@@ -1,7 +1,7 @@
 /**
  * GSM模块的基础命令操作
  *
- * @author 王特
+ * @author 王特、矫东航
  */
 
 #ifndef __USR_GSM_BASE_H__
@@ -15,12 +15,15 @@ bool GSM_AT_Only(char *data);
 // 发送AT命令，并保存接收数据
 bool GSM_AT_Receive(char *data, GSM_RECEIVE_RECORD *pReceive);
 // 发送AT命令，并期望接收recall数据
-bool GSM_AT_RecallGSM_AT_Recall(char *data, char *waitstr);
+bool GSM_AT_Recall(char *data, char *waitstr);
 
 // 接收关键字
 void GSM_Receive_KeyWord(void);
 // 接收普通数据
 void GSM_Receive_Data(GSM_RECEIVE_RECORD *Receive);
+// 接收期待字符串
+bool GSM_Receive_Recall(char *waitstr);
+
 // 去除\r\n操作，返回当前Data长度
 int Remove_CR(UINT8* Data, int count);
 
